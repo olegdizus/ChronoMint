c5d9845b42ac23398d6c0cd18de2d1b7608d7f47
Sozinov Alexander <sanzeting@mail.ru>
2018-07-27 18:01:19 +0300

    MINT-1686 #comment load avatar with backend profile service and ipfs upload; load data in updateForm

--- packages/login/network/ProfileService.js
+++ packages/login/network/ProfileService.js
@@ -7,7 +7,7 @@ import EventEmitter from 'events'
 import axios from 'axios'
 import { store } from '@chronobank/core-dependencies/configureStore'
 
-const PROFILE_BACKEND_REST_URL = 'https://backend.profile.tp.ntr1x.com'
+const PROFILE_BACKEND_REST_URL = 'http://localhost:3000/'
 const basePath = '/api/v1'
 const GET_PERSONS_REST = `${basePath}/security/persons/query`
 const GET_SIGNATURE_REST = `${basePath}/security/signin/signature`
@@ -20,6 +20,8 @@ const CONFIRM_LEVEL_2 = `${basePath}/security/me/profile/level2/confirm`
 const VALIDATE_LEVEL_2_PHONE = `${basePath}/security/me/profile/level2/validate/phone`
 const VALIDATE_LEVEL_2_EMAIL = `${basePath}/security/me/profile/level2/validate/email`
 const PROFILE_NOTIFICATIONS = `${basePath}/security/me/profile/notifications`
+
+const MEDIA_IMAGE_UPLOAD = `${basePath}/media/image/upload`
 
 const PURPOSE_VALUE = 'exchange'
 
@@ -202,6 +204,25 @@ class ProfileService extends EventEmitter {
       name,
       value,
     }, this.withAuthorization(token))
+
+    return data
+  }
+
+  async avatarUpload (file, token) {
+    const service = this.getServerProvider()
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    const { data } = await service.post(
+      MEDIA_IMAGE_UPLOAD,
+      formData,
+      this.withAuthorization(token, {
+        headers: {
+          'content-type': 'multipart/form-data',
+        },
+      })
+    )
 
     return data
   }
